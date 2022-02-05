b4558921be90af2f5a90cb25fe7dbe52e9003305
Karen Abrahamyan <juvelist.g@gmail.com>
2022-02-05 19:13:01 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let logIn = document.getElementById('logIn');
+let loginWrapper = document.getElementById('loginWrapper');
+let loginSection = document.getElementById('loginSection');
+let sidebarLogin = document.getElementById('sidebarLogin');
+let sidebarTooltip = document.getElementById('sidebarTooltip');
+
+function toggleLoginTooltip() {
+  sidebarTooltip.classList.toggle('active');
+}
+
+function openLoginSection() {
+  loginWrapper.classList.add('show');
+  loginSection.classList.add('show');
+  document.body.classList.add('scroll-off');
+}
+
+function closeLoginSection(e) {
+  if(e.target !== e.currentTarget) return;
+  loginWrapper.classList.remove('show');
+  loginSection.classList.remove('show');
+  document.body.classList.remove('scroll-off');
+}
+
+logIn.addEventListener("click", openLoginSection);
+loginWrapper.addEventListener("click", closeLoginSection);
+
+sidebarLogin.addEventListener("click", toggleLoginTooltip);
